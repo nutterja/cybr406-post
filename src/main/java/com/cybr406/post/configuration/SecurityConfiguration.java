160438fd9f4c9d66f6a851a7a26411126bcc4b31
Jacob Nutter <jacobnutter1@gmail.com>
2020-04-11 22:17:33 -0500

    post hw linking to account

--- src/main/java/com/cybr406/post/configuration/SecurityConfiguration.java
+++ src/main/java/com/cybr406/post/configuration/SecurityConfiguration.java
@@ -1,8 +1,10 @@
 package com.cybr406.post.configuration;
 
+import com.cybr406.post.security.user.AccountAuthenticationProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,6 +16,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     H2SecurityConfigurer h2SecurityConfigurer;
+
+    @Autowired
+    private void configureGlobal(
+            AuthenticationManagerBuilder auth,
+            AccountAuthenticationProvider provider) throws Exception {
+        auth.authenticationProvider(provider);
+    }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
--- src/main/java/com/cybr406/post/security/user/AccountAuthenticationProvider.java
+++ src/main/java/com/cybr406/post/security/user/AccountAuthenticationProvider.java
@@ -1,24 +1,66 @@
 package com.cybr406.post.security.user;
 
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class AccountAuthenticationProvider implements AuthenticationProvider {
+
+    private static final ParameterizedTypeReference<List<String>> typeref = new ParameterizedTypeReference<List<String>>() {};
+
+    @Autowired
+    private WebClient webClient;
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         // Update this method to verify user details using the Account microservice.
         // You will need to use the WebClient class to make calls to /check-user in Account
-        return null;
+       try {
+           List<SimpleGrantedAuthority> roles = webClient.get()
+                   .uri("/check-user")
+                   .headers(headers -> headers.setBasicAuth("post", "post"))
+                   .header("x-username", authentication.getName())
+                   .header("x-password", authentication.getCredentials().toString())
+                   .retrieve()
+                   .bodyToMono(typeref)
+                   .blockOptional(Duration.ofSeconds(5))
+                   .orElseThrow(() -> new Exception("no response from account"))
+                   .stream()
+                   .map(SimpleGrantedAuthority::new)
+                   .collect(Collectors.toList());
+
+           return new UsernamePasswordAuthenticationToken(
+                   authentication.getName(),
+                   authentication.getCredentials(),
+                   roles);
+
+
+
+       } catch (Exception e) {
+           return null;
+       }
+
+
     }
 
     @Override
     public boolean supports(Class<?> authentication) {
         // Update this method to indicate this AuthenticationProvider supports UsernamePasswordAuthenticationToken
-        return false;
+
+
+        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
     }
 
 }
